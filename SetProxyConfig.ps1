c0e37eb4d1e11994154feb9ad87a5a2b3ecf83f6
jdhitsolutions <jhicks@jdhitsolutions.com>
2026-09-14 16:48:49 -0400

    added proxy config script

---
+++ SetProxyConfig.ps1
@@ -0,0 +1,21 @@
+#dot source this script
+$global:proxyAddress = 'http://172.31.245.222:8888'
+
+if (-Not $global:proxyUser -AND (Test-Path c:\proxy.txt)) {
+    $global:proxyInfo = ((Get-Content c:\proxy.txt).split(":",3)).trim()
+    $global:proxyUser = $global:proxyInfo[0]
+    $global:proxyPassword = $global:proxyInfo[1]
+}
+Try {
+    $v = Get-Variable proxyUser,proxyPassword -scope Global -errorAction Stop
+    $global:psecpasswd = ConvertTo-SecureString $global:proxyPassword -AsPlainText -Force
+    $global:proxyCredential = [System.Management.Automation.PSCredential]::New($global:proxyUser, $global:psecpasswd)
+    $global:wp = [System.Net.WebProxy]::new($global:proxyAddress)
+    $global:wp.Credentials = $global:proxyCredential
+    $global:webclient = New-Object System.Net.Webclient
+    $global:webclient.proxy = $global:wp
+    [System.Net.WebRequest]::DefaultWebProxy = $global:wp
+    }
+Catch {
+    Write-Warning "missing proxy information"
+}
