4a21d018b003a1752a2662979a61264085216484
jdhitsolutions <jhicks@jdhitsolutions.com>
2026-09-16 16:59:50 -0400

    Updated Get-LogUsage scripts

--- Get-LogUsageV7.ps1
+++ Get-LogUsageV7.ps1
@@ -1,10 +1,14 @@
-#requires -version 5.1
+#requires -version 7.6
 #requires -RunAsAdministrator
 
+<#
+This may not necessarily be the fastest or best way to get this information,
+but it demonstrates using a PowerShell 7 feature in a script file.
+#>
 Param([string]$LogName = "*")
 
 Get-WinEvent -listLog $LogName | Where RecordCount -gt 0 |
-Foreach-Object  {
+Foreach-Object -parallel {
     Select-Object -InputObject $_ -property LogName,RecordCount,LogMode,
     @{Name = "SizeKB";Expression = {$_.FileSize/1kb -as [int]}},
     @{Name = "FileSizeKB";Expression = {$_.MaximumSizeInBytes/1kb -as [int]}},
