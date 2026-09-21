7d9786db52ef2292277224fdded107e97e46f34c
jdhitsolutions <jhicks@jdhitsolutions.com>
2026-09-21 13:52:06 -0400

    updated Configure-Eventlogs.ps1

--- Configure-EventLogs.ps1
+++ Configure-EventLogs.ps1
@@ -1,29 +1,36 @@
 #requires -version 5.1
 
-#configure event log size
-#all parameters are mandatory
-#this script file is intended to be run remotely using Invoke-Command
-Param(
+#configure event log size and mode
+#this script file is intended to be run remotely using Invoke-Command.
+
+param(
     [Parameter(Mandatory)]
-    [string]$LogName,
+    [string[]]$LogName,
     [Parameter(
         Mandatory,
-        HelpMessage = "Enter a value between 64KB and 4GB. The value must be divisible by 64KB"
+        HelpMessage = 'Enter a value between 64KB and 4GB. The value must be divisible by 64KB'
     )]
     [int64]$MaximumSize,
-    [ValidateSet("Circular","Retain","AutoBackup")]
-    [string]$LogMode = "Circular"
+    [ValidateSet('Circular', 'Retain', 'AutoBackup')]
+    [string]$LogMode = 'Circular',
+    [switch]$Passthru
 )
 
-Try {
-    $log = Get-WinEvent -ListLog $LogName -ErrorAction Stop
-    if ($log) {
-        $log.MaximumSizeInBytes = $MaximumSize
-        $log.LogMode = $LogMode
-        $log.SaveChanges()
+foreach ($logItem in $logName) {
+    try {
+        $log = Get-WinEvent -ListLog $logItem -ErrorAction Stop
+        if ($log) {
+            $log.MaximumSizeInBytes = $MaximumSize
+            $log.LogMode = $LogMode
+            $log.SaveChanges()
+        }
+        if ($Passthru) {
+            Get-WinEvent -ListLog $LogItem |
+            Select-Object LogName, MaximumSizeInBytes, LogMode,
+            @{Name = 'Computername'; Expression = { $env:COMPUTERNAME }}
+        }
     }
-    Get-WinEvent -ListLog $LogName
-}
-Catch {
-    Write-Warning "[$($env:computername)] Failed to configure the specified event log, $LogName. $($_.Exception.Message)" -ErrorAction Stop
+    catch {
+        Write-Warning "[$($env:computername)] Failed to configure the specified event log, $LogName. $($_.Exception.Message)" -ErrorAction Stop
+    }
 }
